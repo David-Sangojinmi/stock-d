dd8d1f3f9fb0251ee804484080fd68a0bd0ab1ad
David-Sangojinmi <dsangojinmi26@gmail.com>
2020-08-04 10:22:20 +0100

    Updates

---
+++ precache-manifest.05bcf5094ccbd18f500c45632ed9fbd8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59ded5d54a3da5e1a39fff026caea559",
+    "url": "/David-Sangojinmi/stock-d/index.html"
+  },
+  {
+    "revision": "d423bccae25de4adf324",
+    "url": "/David-Sangojinmi/stock-d/static/css/main.00c46bf3.chunk.css"
+  },
+  {
+    "revision": "cc4a7fe4023bc19cb096",
+    "url": "/David-Sangojinmi/stock-d/static/js/2.2ca8d8da.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/David-Sangojinmi/stock-d/static/js/2.2ca8d8da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d423bccae25de4adf324",
+    "url": "/David-Sangojinmi/stock-d/static/js/main.714646fe.chunk.js"
+  },
+  {
+    "revision": "8520e328c254761a65ea",
+    "url": "/David-Sangojinmi/stock-d/static/js/runtime-main.dcd53c91.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/David-Sangojinmi/stock-d/static/media/logo.5d5d9eef.svg"
+  }
+]);
