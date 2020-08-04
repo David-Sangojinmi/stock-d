7f5da6ab88481f563ab0accdfbdc643e25bb0474
David-Sangojinmi <dsangojinmi26@gmail.com>
2020-08-04 10:35:29 +0100

    Updated

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-import React from 'react';
+import React from "react";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Welcome to React</h2>
-        <p>Now you've gotten started.</p>
-      </div>
-    </div>
-  );
+    return (
+        <section id="home" className="home-banner-01 bg-no-repeat bg-cover bg-fixed home-bg tint-bg-1">
+            <div className="container">
+                <div className="row full-screen align-items-center">
+                    <div className="col-md-12 col-lg-9 text-left">
+                        <div className="home-text">
+                            <h1>
+                                Stock<span id="coloured">'</span>d
+                            </h1>
+                            <h2>Easily track the metrics of your favourite companies</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
 }
 
 export default App;
