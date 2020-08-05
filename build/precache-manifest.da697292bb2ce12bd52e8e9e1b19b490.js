9a6ccb4987bc46d9f7fcbb5dfa1ee8b09800fdb8
David-Sangojinmi <dsangojinmi26@gmail.com>
2020-08-05 18:00:52 +0100

    Merged branch files

---
+++ build/precache-manifest.da697292bb2ce12bd52e8e9e1b19b490.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79fc9401fad9edfdd5787b99cbd761c5",
+    "url": "/stock-d/index.html"
+  },
+  {
+    "revision": "78bd46a333666ebecb9b",
+    "url": "/stock-d/static/css/main.00c46bf3.chunk.css"
+  },
+  {
+    "revision": "cc4a7fe4023bc19cb096",
+    "url": "/stock-d/static/js/2.2ca8d8da.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/stock-d/static/js/2.2ca8d8da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78bd46a333666ebecb9b",
+    "url": "/stock-d/static/js/main.9c119709.chunk.js"
+  },
+  {
+    "revision": "76dc6387674f2fae3ab8",
+    "url": "/stock-d/static/js/runtime-main.7919a6c4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/stock-d/static/media/logo.5d5d9eef.svg"
+  }
+]);
